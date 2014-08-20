1b9f3237def1b831fffa147e0f04a54e2743b79d
retrogamingcollection <retrogamingcollection@collection.com>
2014-08-20 22:36:23 +0200

    Initial commit for mapping project

---
+++ retro-gaming-collection-mapping/src/main/java/org/retro/collection/mapping/Console.java
@@ -0,0 +1,12 @@
+package org.retro.collection.mapping;
+/**
+ * Mapping class for Console 
+ * @author louise
+ *
+ */
+public class Console {
+
+	private String name;
+	
+	private Company company;
+}
